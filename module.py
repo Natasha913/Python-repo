252052358073c3654cd7d1195fd6a9fd467b8cf7
Natasha913 <78008182+Natasha913@users.noreply.github.com>
2026-01-16 17:44:08 -0600

    Add files via upload
    section 3

---
+++ module.py
@@ -0,0 +1,11 @@
+#type 1
+number_to_check = int(input("What is the number you want to check?"))
+
+print(number_to_check % 2)
+
+#type 2
+number_to_check = int(input("What is the number you want to check?"))
+if number_to_check % 2 == 0:
+    print("Even")
+else:
+    print("odd")
