bd122c24de828e0b09104c5f4cbef2853df07a71
Natasha913 <78008182+Natasha913@users.noreply.github.com>
2026-01-20 15:21:38 -0600

    Add files via upload

---
+++ section 4/list.py
@@ -0,0 +1,5 @@
+fruits_name =["Mango","Orange","Banaana"]
+fruits_name[2]="Banana"
+fruits_name.append("Apple")
+fruits_name.extend(["Peace","Grape","Pomegranate"])
+print (fruits_name)
---
+++ section 4/rock_paper_scissors.py
@@ -0,0 +1,23 @@
+import random
+rock = "ROCK"
+paper = "PAPER"
+scissors ="SCISSORS"
+
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for scissors."))
+
+computer_choice = random.randint(0,2)
+print(f"Computer chose {computer_choice}")
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number.You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print ("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print ("You lose!")
+elif computer_choice > user_choice:
+    print ("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice==user_choice:
+    print("It's a draw!")
